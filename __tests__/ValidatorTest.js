5580d7c616958af27becd88d1bdf284a6192384b
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-10 14:26:12 +0900

    test(ValidatorTest): 테스트 기대 값 오류 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,20 +1,13 @@
+import { ERROR } from "../constants.js";
 import { Validator } from "../src/Model/Validator.js"; // 경로 주의!
 
 describe("Validator - 구입금액 검증 기능 단위 테스트", () => {
-  test("빈 입력 값", () => {
-    const input = "";
-
-    expect(() => {
-      Validator.amount(input);
-    }).toThrow("[ERROR]");
-  });
-
   test("500원 단위로 떨어지지 않는 수", () => {
     const input = "1300";
 
     expect(() => {
       Validator.amount(input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.NOT_DIVISIBLE);
   });
 
   test("음수", () => {
@@ -22,7 +15,7 @@ describe("Validator - 구입금액 검증 기능 단위 테스트", () => {
 
     expect(() => {
       Validator.amount(input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.NOT_POSITIVE);
   });
 });
 
@@ -32,7 +25,7 @@ describe("Validator - 당첨 번호 검증 기능 단위 테스트", () => {
 
     expect(() => {
       Validator.winningNum(input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.WINNING_COUNT_OUT_OF_RANGE);
   });
 
   test("중복되는 번호", () => {
@@ -40,35 +33,26 @@ describe("Validator - 당첨 번호 검증 기능 단위 테스트", () => {
 
     expect(() => {
       Validator.winningNum(input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.WINNING_NUM_SAME);
   });
 
   test("1~30 사이의 범위를 벗어난 숫자", () => {
-    const input = "27,28,29,30,31";
+    const input = [27, 28, 29, 30, 31];
 
     expect(() => {
       Validator.winningNum(input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.NUMBER_OUT_OF_RANGE);
   });
 });
 
 describe("Validator - 보너스 번호 검증 기능 단위 테스트", () => {
-  test("빈 값", () => {
-    const winningNum = "1,2,3,4,5";
-    const input = "";
-
-    expect(() => {
-      Validator.bonusNum(winningNum, input);
-    }).toThrow("[ERROR]");
-  });
-
   test("1~30 사이의 숫자가 아닌 값", () => {
     const winningNum = "1,2,3,4,5";
     const input = "35";
 
     expect(() => {
       Validator.bonusNum(winningNum, input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.NUMBER_OUT_OF_RANGE);
   });
 
   test("당첨 번호와 중복되는 수", () => {
@@ -77,6 +61,6 @@ describe("Validator - 보너스 번호 검증 기능 단위 테스트", () => {
 
     expect(() => {
       Validator.bonusNum(winningNum, input);
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR.BONUS_DUPLICATE);
   });
 });
