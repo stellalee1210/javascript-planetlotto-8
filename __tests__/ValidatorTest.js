9cd60e8807858c453609072bd532d7d5444acf06
stellalee1210 <yeobinlee1210@gmail.com>
2026-01-10 14:32:13 +0900

    test(Validator): 테스트 오류 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -3,7 +3,7 @@ import { Validator } from "../src/Model/Validator.js"; // 경로 주의!
 
 describe("Validator - 구입금액 검증 기능 단위 테스트", () => {
   test("500원 단위로 떨어지지 않는 수", () => {
-    const input = "1300";
+    const input = 1300;
 
     expect(() => {
       Validator.amount(input);
@@ -11,7 +11,7 @@ describe("Validator - 구입금액 검증 기능 단위 테스트", () => {
   });
 
   test("음수", () => {
-    const input = "-500";
+    const input = -500;
 
     expect(() => {
       Validator.amount(input);
@@ -21,7 +21,7 @@ describe("Validator - 구입금액 검증 기능 단위 테스트", () => {
 
 describe("Validator - 당첨 번호 검증 기능 단위 테스트", () => {
   test("번호가 5개 미만으로 입력", () => {
-    const input = "1,2,3,4";
+    const input = [1, 2, 3, 4];
 
     expect(() => {
       Validator.winningNum(input);
@@ -29,7 +29,7 @@ describe("Validator - 당첨 번호 검증 기능 단위 테스트", () => {
   });
 
   test("중복되는 번호", () => {
-    const input = "1,2,3,3,5";
+    const input = [1, 2, 3, 3, 5];
 
     expect(() => {
       Validator.winningNum(input);
@@ -47,20 +47,20 @@ describe("Validator - 당첨 번호 검증 기능 단위 테스트", () => {
 
 describe("Validator - 보너스 번호 검증 기능 단위 테스트", () => {
   test("1~30 사이의 숫자가 아닌 값", () => {
-    const winningNum = "1,2,3,4,5";
-    const input = "35";
+    const winningNum = [1, 2, 3, 4, 5];
+    const input = 35;
 
     expect(() => {
-      Validator.bonusNum(winningNum, input);
+      Validator.bonusNum(input, winningNum);
     }).toThrow(ERROR.NUMBER_OUT_OF_RANGE);
   });
 
   test("당첨 번호와 중복되는 수", () => {
-    const winningNum = "1,2,3,4,5";
-    const input = "5";
+    const winningNum = [1, 2, 3, 4, 5];
+    const input = 5;
 
     expect(() => {
-      Validator.bonusNum(winningNum, input);
+      Validator.bonusNum(input, winningNum);
     }).toThrow(ERROR.BONUS_DUPLICATE);
   });
 });
